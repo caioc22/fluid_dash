291fc151832ce23bcb83c2bcc308765180f97331
caioc22 <caioccnh@gmail.com>
2023-01-02 09:40:02 -0300

    start fast api

---
+++ templates/script.js
@@ -0,0 +1,8 @@
+class Test extends React.Component{
+    render(){
+        return (
+            <p>Lorem</p>
+        )
+    }
+}
+ReactDOM.render(<Test/>, document.getElementById('react-container'))
